b2723580c48910577a7aefb26bad2577d9ee9f3d
danmaheruk <dan.maher@me.com>
2019-01-22 12:58:14 +0000

    resource generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :portfolios
   resources :blogs
-  get '/home/home'
+  get '/home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
