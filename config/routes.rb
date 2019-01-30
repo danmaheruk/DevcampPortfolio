b585c745e2a04815e613bc1f754a8c40504f8192
danmaheruk <dan.maher@me.com>
2019-01-30 14:35:43 +0000

    completed enum scoping

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,11 @@ Rails.application.routes.draw do
   resources :portfolios
   resources :blogs
   get '/home/index'
+
+  resources :blogs do
+  	member do
+  	 get :toggle_status
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
