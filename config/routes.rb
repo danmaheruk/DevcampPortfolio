48dc27d5a61387a95a30700db1db35585046a671
danmaheruk <dan.maher@me.com>
2019-02-07 21:23:25 +0000

    relationships blogs and categorys

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 	root 'page#home'
 
+  get 'portfolio/:id', to: 'portfolio#show', as: 'portfolio_show'
   get '/home', to: 'page#home'
   get '/about-me', to: 'page#about'
   get '/contact', to: 'page#contact'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_07_104703) do
+ActiveRecord::Schema.define(version: 2019_02_07_211039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,16 @@ ActiveRecord::Schema.define(version: 2019_02_07_104703) do
     t.string "slug"
     t.integer "status", default: 0
     t.bigint "topic_id"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_blogs_on_category_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
     t.index ["topic_id"], name: "index_blogs_on_topic_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -74,5 +82,6 @@ ActiveRecord::Schema.define(version: 2019_02_07_104703) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "blogs", "categories"
   add_foreign_key "blogs", "topics"
 end
