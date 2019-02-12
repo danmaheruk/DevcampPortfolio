464aeb3482a845db3c668263aa8538314d3346fc
danmaheruk <dan.maher@me.com>
2019-02-12 11:20:52 +0000

    set defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,11 @@ class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image 
 
 	scope :react_scope, -> { where(subtitle: "React JS") }
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "http://via.placeholder.com/600x400"
+		self.thumb_image ||= "http://via.placeholder.com/150x150"
+	end
 end
