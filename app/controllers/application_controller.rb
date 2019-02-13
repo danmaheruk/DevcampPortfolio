588f2a2f1bc266fc9401fef62f82533e0ef19680
danmaheruk <dan.maher@me.com>
2019-02-13 10:05:07 +0000

    rubygem prototype

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	include DeviseWhitelist
+	
+	before_action :set_copyright
+
+	def set_copyright
+		@copyright = DevcampViewTool::Renderer.copyright 'Daniel Maher', 'All Rights Reserved'
+	end
+end
+
+module DevcampViewTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
 end
